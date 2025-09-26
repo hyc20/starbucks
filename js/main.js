ebac1a7dd422ba2b844eb0b6e6d6f130fabbccef
hyc20 <hyc104389@gmail.com>
2025-09-27 06:58:47 +0900

    lodash, gsap 라이브러리 추가

--- js/main.js
+++ js/main.js
@@ -15,3 +15,30 @@ searchInputEl.addEventListener('blur',function(){
   searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('header .badges');
+
+// window.addEventListener('scroll', function(){
+//   console.log('scoll');
+// })
+
+//._throttle(함수, 시간)
+window.addEventListener('scroll',_.throttle(function(){
+  console.log(window.scrollY);
+
+  if(window.scrollY > 500 ){
+    //배지 숨기기
+    // badgeEl.style.display='none';
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity : 0,
+      display : 'none'
+    })
+  }else{
+    //배지 숨기기
+    // badgeEl.style.display='block'
+    gsap.to(badgeEl, .6, {
+      opacity : 1,
+      display : 'block' 
+    })
+  }
+}, 300));
